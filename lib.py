fb7c2345100495f70d4c0263b2bd8f088e12d663
Hitesh Yadav <hiteshyadav3607@gmail.com>
2024-10-09 00:48:58 +0530

    library version updated

---
+++ lib.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import requests
+import motor
+import telethon
+import pymongo
+import aiogram
+import asyncio
+
+
+print(f"Pandas version: {pd.__version__}")
+print(f"Numpy version: {np.__version__}")
+print(f"Requests version: {requests.__version__}")
+print(f"Motor version: {motor.version}")
+print(f"Telethon version: {telethon.__version__}")
+print(f"Pymongo version: {pymongo.__version__}")
+print(f"aiogram version: {aiogram.__version__}")
+print(f"Asyncio version: {asyncio.__version__}")
